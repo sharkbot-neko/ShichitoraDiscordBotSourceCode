7e1ec680d52e8a7d27639f6e6f08b971b4b22010
七虎なる <naruyko769.naru7691@gmail.com>
2025-12-25 14:16:43 +0900

    Update start.js

--- app/start.js
+++ app/start.js
@@ -1,1 +1,31 @@
+// Sharding
+// node start.js
 
+import { ShardingManager } from 'discord.js';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import 'dotenv/config';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const manager = new ShardingManager(path.join(__dirname, 'server.js'), {
+  token: process.env.TOKEN,
+  totalShards: 3,
+  respawn: true,
+  shardArgs: ['--no-warnings'],
+  execArgv: ['--max-old-space-size=4096'], 
+  timeout: -1,
+});
+
+manager.on('shardCreate', (shard) => {
+  console.log(`Shard ${shard.id} を起動しました`);
+});
+
+manager.spawn({
+  amount: 3,
+  delay: 5000,
+  timeout: -1
+})
+  .then(() => console.log('全てのシャードが起動しました'))
+  .catch(console.error);
